c5386b990ecbb8b507101ddae1656b3c65f02d28
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-05-21 21:53:37 -0500

    Update 1.2 StringPermutation.java

--- 1.2 StringPermutation.java
+++ 1.2 StringPermutation.java
@@ -30,3 +30,48 @@ public class SortAndCompare {
 	}
 }
 
+/*----MAIN FUNCTION---------------------------------------------------------*/
+
+
+public class Entry {
+	
+	
+	public static void main(String args[]) {
+		String str1 = "abbccdd";
+		String str2 = "ddabcbc";
+		String str3 = "abcde";
+		String str4 = "cdefg";
+		String str5 = "abcde";
+		String str6 = "abcdefg";
+		
+		System.out.println("Method - 1 : Sort and Equate \nMethod Complexity : O(log(n)))\n");
+		
+		SortAndCompare sac1 = new  SortAndCompare(str1, str2);
+		Boolean flag1 = sac1.checkEquality();
+		SortAndCompare sac2 = new  SortAndCompare(str3, str4);
+		Boolean flag2 = sac2.checkEquality();
+		SortAndCompare sac3 = new  SortAndCompare(str5, str6);
+		Boolean flag3 = sac3.checkEquality();
+		
+		
+		if(flag1) {
+			System.out.println(str1+" and "+str1+" are permutations of each other");
+		}else {
+			System.out.println(str1+" and "+str1+" are not permutations of each other");
+		}
+		
+		if(flag2) {
+			System.out.println(str3+" and "+str4+" are permutations of each other");
+		}else {
+			System.out.println(str3+" and "+str4+" are not permutations of each other");
+		}
+		
+		if(flag3) {
+			System.out.println(str5+" and "+str6+" are permutations of each other");
+		}else {
+			System.out.println(str5+" and "+str6+" are not permutations of each other");
+		}
+	}
+}
+
+
