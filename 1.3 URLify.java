01f7b3e208eeb43d54f7388b4d71446b4500a3d2
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-05-22 00:53:03 -0500

    Update 1.3 URLify.java

--- 1.3 URLify.java
+++ 1.3 URLify.java
@@ -17,5 +17,66 @@ public class Entry {
 }
 
 /** Core Functional Classes**/
+package Entry;
+
+public class URLify {
+	String str;
+	int strLength;
+	String url;
+	public URLify(String str, int strLength) {
+		// TODO Auto-generated constructor stub
+		this.str = str;
+		this.strLength = strLength;
+		
+	}
+	
+	public String makeURLfromString() {
+		//Declare variables
+		int numberOfSpaces = 0;
+		
+		//Convert the string into character array
+		char[] strArr = str.toCharArray();
+		System.out.println("\nLength of Array : "+strArr.length);
+		
+		int spaceChar = " ".charAt(0);
+		
+		
+		//Check the number of white spaces in the string
+		//O[n]
+		
+		for(int j=0; j < strLength; j++) {
+			if((strArr[j]) == " ".charAt(0)) {
+				System.out.println(strArr[j]);
+				numberOfSpaces++;
+			}
+		}
+		
+		System.out.println("\nNumber of spaces in word : "+numberOfSpaces);
+		
+		//Calculate the difference diff from where backward traversal of 
+		//String Array is to be started. '%20' takes 3 characters.
+		
+		
+		int diff = numberOfSpaces*3 - numberOfSpaces;
+		System.out.println("\nInitial Calculated difference : "+diff);
+		
+		for(int i=strLength-1; i>=0; i--) {			
+			//System.out.println("Inside For Loop and the index is : "+i);
+			if(strArr[i]!=spaceChar) {
+				//System.out.println("Inside String Array");
+				strArr[i + diff] = strArr[i];
+			}else {
+				strArr[i+ diff]='0';
+				strArr[i+ (diff - 1)]='2';
+				strArr[i +(diff - 2)]='%';
+				diff = diff - numberOfSpaces;
+				//System.out.println("New Calculated difference : "+diff);
+			}
+		}
+		
+		return new String(strArr);	
+	}
+	
+}
 
 
