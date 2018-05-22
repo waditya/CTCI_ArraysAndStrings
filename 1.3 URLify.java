2edb931c6638c9b10b0fc9a25e17cc6a03c7cd4d
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-05-22 00:52:23 -0500

    Update 1.3 URLify.java

--- 1.3 URLify.java
+++ 1.3 URLify.java
@@ -1,1 +1,18 @@
-/*Entry Class*/
+/*Entry Class with Main Method*/
+package Entry;
+
+public class Entry {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str1 = "MR JOHN SMITH    ";
+		int str1_length = 13;
+		URLify word = new URLify(str1, str1_length);
+		System.out.println("\n Word is : "+str1);
+		System.out.println("\n Length of Word is : "+str1_length);
+		System.out.println("\nURL of word is : "+word.makeURLfromString());
+	}
+
+}
+
