37329069bfd5142eb5fa38df0dada9e9f0e2fb85
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-06-09 17:04:39 -0500

    Update 1.6 String Compression.java

--- 1.6 String Compression.java
+++ 1.6 String Compression.java
@@ -1,1 +1,16 @@
+package Master;
+
+import businessLogic.CompressString;
+
+public class Entry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		CompressString cs = new CompressString();
+		System.out.println(cs.compressBad("aabcccccaa"));;
+		
+
+	}
+
+}
 
